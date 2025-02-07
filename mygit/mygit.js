7bec502d1d6eb7af5523918ac16c51707a77844d
saipr0 <saiprabhat.05@gmail.com>
2025-02-08 00:09:34 +0530

    building git - completed

--- mygit/mygit.js
+++ mygit/mygit.js
@@ -44,12 +44,28 @@
     }
     return history;
   };
+
   // Actual command -> $git log
-  
+
   Git.prototype.checkout = function (branchName) {
-    
-  }
-  
+    for (let i = 0; i < this.branches.length; i++) {
+      if (this.branches[i].name === branchName) {
+        console.log("Switched to existing branch: " + branchName);
+        this.HEAD = this.branches[i];
+        return this;
+      }
+    }
+
+    let newBranch = new Branch(branchName, this.HEAD.commit);
+    this.branches.push(newBranch);
+    this.HEAD = newBranch;
+
+    console.log("Switched to new branch: " + branchName);
+    return this;
+  };
+
+  // Actual command -> $git checkout existing-branch and $git checkout -b new-branch
+
   function Branch(name, commit) {
     this.name = name;
     this.commit = commit;
@@ -79,3 +95,32 @@ repo.checkout("master");
 console.assert(repo.HEAD.name === "master");
 repo.checkout("testing");
 console.assert(repo.HEAD.name === "testing");
+
+console.log("3. Branches test");
+
+repo = new Git("test");
+repo.commit("Initial commit");
+repo.commit("Change 1");
+
+// Maps the array of commits into a string of commit ids.
+// For [C2, C1, C3], it returns "2-1-0"
+
+function historyToIdMapper(history) {
+  let ids = history.map(function (commit) {
+    return commit.id;
+  });
+  return ids.join("-");
+}
+
+console.assert(historyToIdMapper(repo.log()) === "1-0");
+
+repo.checkout("testing");
+repo.commit("Change 3");
+
+console.assert(historyToIdMapper(repo.log()) === "2-1-0")
+
+repo.checkout("master")
+console.assert(historyToIdMapper(repo.log()) === "1-0")
+
+repo.commit("Change 3")
+console.assert(historyToIdMapper(repo.log()) === "3-1-0")
